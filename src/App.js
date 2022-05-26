febda0b7a11330e3e97497473ccdaddd23ea45cd
Siyovush <paraparadox@protonmail.com>
2022-05-27 02:57:19 +0500

    Set up ImageCard component. Fetch images from pixabay

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
+import { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" className="w-full"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            4000
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-      </div>
+    <div className="container mx-auto">
+      {
+        isLoading
+          ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+          : (
+            <div className="grid grid-cols-3 gap-4">
+              {
+                images.map(image => (
+                  <ImageCard key={ image.id } image={ image } />
+                ))
+              }
+            </div>
+          )
+      }
     </div>
   );
 }
